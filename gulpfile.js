27fadc23fec3a76049983e16b8a01cd6f4050376
David <hrdavidl@gmail.com>
2016-05-26 11:36:09 -0400

    How to Manage Your CSS and JS
    - Moved styles to a styles folder within our public folder, we can include this folder as the href for our style sheet.
    
    - We also used SASS and Gulp to form our style sheets as well as setup versioning with SASS to allow cache busting.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	//versioning the file will allows cahce busting
+    mix.sass('app.scss').version('css/app.css');
 });
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,37 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+
         <meta charset="utf-8">
+
         <title>Document</title>
+
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <style>
-            html, body {
-                height: 100%;
-            }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-            }
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
 
