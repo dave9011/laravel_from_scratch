3621aeca4d3f646609324314e8994c41a204c7be
David <hrdavidl@gmail.com>
2016-05-26 09:49:37 -0400

    *Introduction to layout files
    - Add out template file, layout.blade, which we can extend other views from
    - Move duplicate code from child views to out parent view layout.blade
    - Make controller return a view when we call for '/about'
    - Add a footer section to about page only to demonstrate how we can use sections selectively in our child views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <h1>The About Page goes here!</h1>
+@stop
+
+@section('footer')
+    <script type="text/javascript">
+        alert('Hello from the ABOUT page');
+    </script>
+@stop
